d5c1824e2d1203edc5eaab1d226b9769e393ae74
IlyaKhmialkov <gromokotgrom@gmail.com>
2024-12-12 13:04:10 +0300

    feat: implement subscriptions to trainers

--- course_project/src/components/trainersCards/trainersCards.tsx
+++ course_project/src/components/trainersCards/trainersCards.tsx
@@ -1,12 +1,60 @@
 'use client'
 
+import { useClient } from '@/hooks/queryHooks/clients/useClient'
 import { useTrainers } from '@/hooks/queryHooks/trainers/useTrainers'
+import { useSetJWT } from '@/hooks/useSetJWT'
+import { useQueryClient } from '@tanstack/react-query'
+import Cookies from 'js-cookie'
+import { useEffect, useState } from 'react'
+import { MdPersonAdd, MdPersonAddDisabled } from 'react-icons/md'
+import axios from '../../utils/axiosConfig'
 import { Slider } from '../emblaCarousel/slider'
 import { TrainersReport } from '../reports/trainersReport'
 import styles from './trainersCards.module.scss'
 
 export function TrainersCards() {
 	const { data, isLoading } = useTrainers()
+	const [isClientIdValid, setClientIdValid] = useState(true)
+	const queryClient = useQueryClient()
+	const clientId = Cookies.get('clientId') ?? 'not a client'
+
+	useEffect(() => {
+		setClientIdValid(!!(clientId && !isNaN(parseInt(clientId)) && isFinite(parseInt(clientId))))
+	}, [])
+
+	useSetJWT()
+	const client = useClient(parseInt(clientId))
+
+	async function invalidateClientQuery() {
+		queryClient.invalidateQueries({ queryKey: ['client', parseInt(clientId)] })
+	}
+
+	async function subscribeHandler(trainerId: number) {
+		const clientTrainerId = client.data?.trainerId ? client.data.trainerId : null
+		let queryTrainerId: null | number = null
+
+		if (clientTrainerId) {
+			if (clientTrainerId !== trainerId) {
+				alert('you can be subscribed only at one trainer, please unsubscribe your trainer first')
+				return
+			}
+		} else {
+			queryTrainerId = trainerId
+		}
+
+		const queryData = {
+			data: {
+				trainerId: queryTrainerId,
+			},
+		}
+
+		const response = await axios.put(`clients/${client.data?.id}`, queryData)
+		if (response.status === 200) {
+			invalidateClientQuery()
+		} else {
+			alert('error')
+		}
+	}
 
 	return (
 		<div className={styles.trainersCards}>
@@ -35,6 +83,17 @@ export function TrainersCards() {
 												email: <a href={`mailto:${trainer.email}`}>{trainer.email}</a>
 											</p>
 										</div>
+										{isClientIdValid && (
+											<div className={styles.buttonDiv}>
+												<button onClick={() => subscribeHandler(trainer.id)}>
+													{client.data?.trainerId === trainer.id ? (
+														<MdPersonAddDisabled size={30} />
+													) : (
+														<MdPersonAdd size={30} />
+													)}
+												</button>
+											</div>
+										)}
 									</div>
 								</div>
 							))}
